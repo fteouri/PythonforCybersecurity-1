1c15262fada7ec795bcd026ca6dd4d5e6fef1574
Faisal <Teour.Faisal@student.greenriver.edu>
2025-07-30 03:40:31 -0700

    hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 
 print("hello, faisal!")
 print("today is going to be a great day")
-
+print("yes it will be in the gace of god")
+print("how old are you")
+Print("i am 25 years old")
+Print("what is favorite meal")
+print("african food fufu with goat meat")
